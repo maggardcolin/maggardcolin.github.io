34396e1c87667ea7e0c696f2e163d4bee16685a2
maggardcolin <110071999+maggardcolin@users.noreply.github.com>
2024-01-15 01:48:55 -0600

    Final button change for mobile devices.

--- js/makereflexive.js
+++ js/makereflexive.js
@@ -53,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (title.textContent === "Experience" || title.textContent === "Projects") {
             const resultsButton = document.querySelector('.results');
             resultsButton.style.width = "285px";
+            resultsButton.style.marginBottom = '5%';
         }
         
     }
