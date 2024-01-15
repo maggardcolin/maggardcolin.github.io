b8df7da7fe1d351b584eccb4f279d075f9196524
maggardcolin <110071999+maggardcolin@users.noreply.github.com>
2024-01-14 22:25:18 -0600

    Added code to make index page reflexive

--- js/makereflexive.js
+++ js/makereflexive.js
@@ -2,10 +2,26 @@
 function isMobile() {
     const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
     return regex.test(navigator.userAgent);
-  }
+}
+function windowedMode() {
+    const screenWidth = window.screen.width;
+    const windowWidth = window.innerWidth;
+    const threshold = 0.5 * screenWidth;
+
+    // less than half of the max screen width, then set as columns
+    return (
+        (windowWidth < screenWidth - threshold)
+    );
+}
+function handleResize() {
+    const listStructure = document.querySelector('.list-structure');
+    listStructure.style.flexDirection = windowedMode() ? 'column' : '';
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     if (!isMobile()) {
+        window.addEventListener('resize', handleResize);
+        handleResize();
     } else {
         console.log("Mobile device detected");
         const navbarelements = document.querySelectorAll('.navbarelements');
